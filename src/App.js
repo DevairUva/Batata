5fb41940f2ead5e47b539044442e1611714780a9
Devair Martins <devair.martins@gmail.com>
2022-06-09 21:31:10 -0300

    branch

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>DESCANSA, FABIANO!</h1>
+        <p>Vamos testar a branch!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
